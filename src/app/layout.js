250d68da1ae0a06fee32aa80d7d870df7f070d5b
Ephrem <ephshif7@gmail.com>
2024-12-25 22:37:42 +0300

    Implement Clerk authentication: added middleware for Clerk, integrated ClerkProvider in layout, updated Header component for user authentication, and created sign-in and sign-up pages.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,8 @@ import "./globals.css";
 import Header from "./components/Header";
 import { ThemeProvider } from "next-themes";
 import ThemeCom from "./components/ThemeCom";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeModeScript } from "flowbite-react";
 
 export const metadata = {
   title: "Create Next App",
@@ -22,17 +13,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider>
-          <ThemeCom>
-            <Header />
-            {children}
-          </ThemeCom>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <ThemeModeScript />
+        </head>
+
+        <body className="relative">
+          <ThemeProvider>
+            <ThemeCom>
+              <Header />
+              {children}
+            </ThemeCom>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
