c1c14ae75519a8c9ac591d17f389e73dc80f9fe8
Ephrem <ephshif7@gmail.com>
2025-01-31 22:03:02 +0300

    Update car data and homepage, remove unused components
    - Change 'city_mpg' to 'kph' in cars_import.json
    - Remove SearchBar and CustomFilter components from homepage
    - Delete unused PostPage and SearchManufacturer type files
    - Simplify Next.js configuration by removing image domains

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
-import SearchBar from "./components/SearchBar";
-import CustomFilter from "./components/CustomFilter";
+
 import HomeCars from "./components/HomeCars";
 
 export default function HomePage() {
   return (
     <main className="overflow-hidden">
       <Hero />
-
       <div className="mt-12 sm:px-16 px-6 py-4 max-w-[1440px] mx-auto">
         <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-100">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Browse the cars you might like</p>
-        </div>
-
-        <div className="home__filters">
-          <SearchBar />
-
-          <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
-          </div>
         </div>
         <HomeCars />
       </div>
