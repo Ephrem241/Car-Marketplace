ec4ea699b07774539d7d04b67e810ad60d0f73e0
Ephrem <ephshif7@gmail.com>
2025-04-04 22:02:06 +0300

    Refactor Firebase Admin initialization: update import statement and modify initAdmin function to ensure singleton app instance for improved performance.

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -1,15 +1,25 @@
-import { cert, initializeApp as initAdmin } from "firebase-admin/app";
+import {
+  cert,
+  initializeApp as initializeFirebaseAdmin,
+} from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 
-const adminApp = initAdmin({
-  credential: cert({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY
-      ? JSON.parse(`"${process.env.FIREBASE_ADMIN_PRIVATE_KEY}"`)
-      : undefined,
-  }),
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-});
+let app;
 
-export const adminStorage = getStorage(adminApp);
+export function initAdmin() {
+  if (!app) {
+    app = initializeFirebaseAdmin({
+      credential: cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY
+          ? JSON.parse(`"${process.env.FIREBASE_ADMIN_PRIVATE_KEY}"`)
+          : undefined,
+      }),
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    });
+  }
+  return app;
+}
+
+export const adminStorage = getStorage(app);
